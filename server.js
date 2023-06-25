9e42b8925ab5a409e605ed6112c0039e62caff02
heddrick <137663748+heddrick@users.noreply.github.com>
2023-06-25 00:19:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('*', (req, res) => {
 });
 
 // Listen and serve.
-const PORT = process.env.PORT || 3000;
+const PORT = 8090;
 server.listen(PORT, () => {
   console.log(`App started on PORT ${PORT}`);
 });
